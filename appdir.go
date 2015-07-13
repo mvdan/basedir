093aa6f21a7de776631ad5043b7264adcd20a355
Daniel Martí <mvdan@mvdan.cc>
2015-07-12 19:34:37 -0700

    Split up XDG stuff into appdir_unix.go

--- appdir.go
+++ appdir.go
@@ -5,35 +5,7 @@ package appdir
 
 import (
 	"errors"
-	"os"
 	"os/user"
-	"path/filepath"
-)
-
-type dirSet struct {
-	dirVar, dirDef   string
-	dirsVar, dirsDef string
-}
-
-var (
-	cache = dirSet{
-		dirVar:  "XDG_CACHE_HOME",
-		dirDef:  ".cache",
-		dirsVar: "",
-		dirsDef: "",
-	}
-	config = dirSet{
-		dirVar:  "XDG_CONFIG_HOME",
-		dirDef:  ".config",
-		dirsVar: "XDG_CONFIG_DIRS",
-		dirsDef: "/etc/xdg",
-	}
-	data = dirSet{
-		dirVar:  "XDG_DATA_HOME",
-		dirDef:  ".local/share",
-		dirsVar: "XDG_DATA_DIRS",
-		dirsDef: "/usr/local/share:/usr/share",
-	}
 )
 
 func userHomeDir() (string, error) {
@@ -44,59 +16,26 @@ func userHomeDir() (string, error) {
 	return curUser.HomeDir, nil
 }
 
-func (ds dirSet) dir() (string, error) {
-	if dir := os.Getenv(ds.dirVar); dir != "" {
-		return dir, nil
-	}
-	home, err := userHomeDir()
-	if err != nil {
-		return "", err
-	}
-	return filepath.Join(home, ds.dirDef), nil
-}
-
-func (ds dirSet) dirs() ([]string, error) {
-	dir, err := ds.dir()
-	if err != nil {
-		return nil, err
-	}
-	dirs := []string{dir}
-	if ds.dirsVar == "" {
-		return dirs, nil
-	}
-	extra := os.Getenv(ds.dirsVar)
-	if extra == "" {
-		extra = ds.dirsDef
-	}
-	for _, path := range filepath.SplitList(extra) {
-		if path == "" {
-			continue
-		}
-		dirs = append(dirs, path)
-	}
-	return dirs, nil
-}
-
 func Cache() (string, error) {
-	return cache.dir()
+	return cache()
 }
 
 func CacheList() ([]string, error) {
-	return cache.dirs()
+	return cacheList()
 }
 
 func Config() (string, error) {
-	return config.dir()
+	return config()
 }
 
 func ConfigList() ([]string, error) {
-	return config.dirs()
+	return configList()
 }
 
 func Data() (string, error) {
-	return data.dir()
+	return data()
 }
 
 func DataList() ([]string, error) {
-	return data.dirs()
+	return dataList()
 }
---
+++ appdir_unix.go
@@ -0,0 +1,94 @@
+/* Copyright (c) 2015, Daniel Martí <mvdan@mvdan.cc> */
+/* See LICENSE for licensing information */
+
+// +build darwin dragonfly freebsd linux nacl netbsd openbsd solaris
+
+package appdir
+
+import (
+	"os"
+	"path/filepath"
+)
+
+type dirSet struct {
+	dirVar, dirDef   string
+	dirsVar, dirsDef string
+}
+
+var (
+	cacheSet = dirSet{
+		dirVar:  "XDG_CACHE_HOME",
+		dirDef:  ".cache",
+		dirsVar: "",
+		dirsDef: "",
+	}
+	configSet = dirSet{
+		dirVar:  "XDG_CONFIG_HOME",
+		dirDef:  ".config",
+		dirsVar: "XDG_CONFIG_DIRS",
+		dirsDef: "/etc/xdg",
+	}
+	dataSet = dirSet{
+		dirVar:  "XDG_DATA_HOME",
+		dirDef:  ".local/share",
+		dirsVar: "XDG_DATA_DIRS",
+		dirsDef: "/usr/local/share:/usr/share",
+	}
+)
+
+func (ds dirSet) dir() (string, error) {
+	if dir := os.Getenv(ds.dirVar); dir != "" {
+		return dir, nil
+	}
+	home, err := userHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(home, ds.dirDef), nil
+}
+
+func (ds dirSet) dirs() ([]string, error) {
+	dir, err := ds.dir()
+	if err != nil {
+		return nil, err
+	}
+	dirs := []string{dir}
+	if ds.dirsVar == "" {
+		return dirs, nil
+	}
+	extra := os.Getenv(ds.dirsVar)
+	if extra == "" {
+		extra = ds.dirsDef
+	}
+	for _, path := range filepath.SplitList(extra) {
+		if path == "" {
+			continue
+		}
+		dirs = append(dirs, path)
+	}
+	return dirs, nil
+}
+
+func cache() (string, error) {
+	return cacheSet.dir()
+}
+
+func cacheList() ([]string, error) {
+	return cacheSet.dirs()
+}
+
+func config() (string, error) {
+	return configSet.dir()
+}
+
+func configList() ([]string, error) {
+	return configSet.dirs()
+}
+
+func data() (string, error) {
+	return dataSet.dir()
+}
+
+func dataList() ([]string, error) {
+	return dataSet.dirs()
+}
