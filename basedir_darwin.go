923cfca8badcb55c71ea5532b3dbead65c90c26b
Daniel Martí <mvdan@mvdan.cc>
2015-07-24 18:58:58 -0700

    Split darwin in its own file

--- basedir_darwin.go
+++ basedir_darwin.go
@@ -1,13 +1,11 @@
 // Copyright (c) 2015, Daniel Martí <mvdan@mvdan.cc>
 // See LICENSE for licensing information
 
-// +build dragonfly freebsd linux netbsd openbsd
-
 package basedir
 
 var (
-	cacheDir = firstGetenv(".cache", "XDG_CACHE_HOME")
-	dataDir  = firstGetenv(".config", "XDG_CONFIG_HOME")
+	cacheDir = firstGetenv("Library/Caches")
+	dataDir  = firstGetenv("Library")
 )
 
 func cache() string {
